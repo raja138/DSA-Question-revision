3425515bfc4ddd37f6deaec7a04a47ce43c77123
raja138 <62994530+raja138@users.noreply.github.com>
2026-02-09 03:18:30 +0000

    sortcolor

---
+++ arrsortcolor.cpp
@@ -0,0 +1,27 @@
+// o pehle aa jayee 1 beech me aur 2 last me
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0,mid=0,high=nums.size()-1;
+
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+    }
+};

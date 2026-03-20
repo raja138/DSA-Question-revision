6ca5c6ccc071b0de29a116bc1f310caf7ba9e754
raja138 <62994530+raja138@users.noreply.github.com>
2026-03-20 08:35:04 +0530

    k length

--- DynamicProgramming/rev1/partitionarrmaxsum.cpp
+++ DynamicProgramming/rev1/partitionarrmaxsum.cpp
@@ -1,7 +1,7 @@
 //index se start krke aage bdho aur phr jo bhi partition aaye
 //usse use krkr 
 //suno simple ho kya rha hai array pe jaao
-//ek ek krke phr array ka partition kro k length ka
+//ek ek krke phr array ka partition kro atmost k length tk ka
 //phr us partition me jo bhi elem max ho usse replace kr do sbko aur sum kro
 //बिल्कुल! जब तुम हर पार्टिशन बना रहे हो, उस हिस्से का मैक्सिमम चुनकर उसे पूरे हिस्से में भरोगे, फिर उस हिस्से का जोड़ निकालोगे। आखिर में, इन सारे हिस्सों के जोड़े मिलाकर तुम्हें पूरा कुल जोड़ मिलेगा, और यही तुम्हारा अंतिम जवाब होगा।
 
